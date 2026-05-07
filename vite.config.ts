59153125d4be8874b9765c6aabed122b3bd4f477
pombadev <11157420+pombadev@users.noreply.github.com>
2026-05-07 21:52:59 +0545

    chore: change PWA registerType to prompt

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
       devOptions: {
         enabled: true
